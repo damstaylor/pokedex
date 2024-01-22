15d112fbcf4bef6128d55d70047409658c04bb84
Damien SABO <damien.sabo@ekino.com>
2024-01-22 17:23:36 +0100

    Add Prettier + config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'standard-with-typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-refresh'
+  ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
