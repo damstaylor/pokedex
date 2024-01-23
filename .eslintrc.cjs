14d1b43a20e9436a69b9fcc91788db49cc0dad8e
Damien SABO <damien.sabo@ekino.com>
2024-01-23 17:37:34 +0100

    chore: add tylelint & fix prettier and eslint with Airbnb code style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,26 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true, jest: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'prettier',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'standard-with-typescript',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'commitlint.config.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'commitlint.config.cjs', 'jest.config.ts', 'vite.config.ts', 'vite-env.d.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: [
-    'prettier',
-    'react',
-    'react-refresh'
-  ],
+  plugins: ['prettier', 'react', 'react-refresh'],
   rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'react/react-in-jsx-scope': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
     '@typescript-eslint/space-before-function-paren': 'off',
   },
-}
+};
