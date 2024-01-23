7b93496d65dcdf4a3a7538b11e091581c5bc4f5f
Damien SABO <damien.sabo@ekino.com>
2024-01-23 19:26:06 +0100

    chore: fix last Prettier issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,37 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true, jest: true },
   extends: [
-    'airbnb',
-    'airbnb-typescript',
     'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parserOptions: {
     project: './tsconfig.json',
   },
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'commitlint.config.cjs', 'jest.config.ts', 'vite.config.ts', 'vite-env.d.ts'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'commitlint.config.cjs',
+    'jest.config.ts',
+    'vite.config.ts',
+    'vite-env.d.ts',
+  ],
   plugins: ['prettier'],
   rules: {
-    'import/extensions': ['error', 'ignorePackages', { 'js': 'never', 'jsx': 'never', 'ts': 'never', 'tsx': 'never' }],
-    'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+    ],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'react/function-component-definition': [
+      'error',
+      { namedComponents: 'arrow-function' },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/space-before-function-paren': 'off',
   },
--- .vite/deps/react.js
+++ .vite/deps/react.js
@@ -1,5 +1,3 @@
-import {
-  require_react
-} from "./chunk-ENWHTCLM.js";
+import { require_react } from './chunk-ENWHTCLM.js';
 export default require_react();
 //# sourceMappingURL=react.js.map
