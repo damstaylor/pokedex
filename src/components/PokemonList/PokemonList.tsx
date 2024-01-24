516f1a443adae5c42d0c6bfed89453378957c857
Damien SABO <damien.sabo@ekino.com>
2024-01-24 11:06:22 +0100

    fix: types for PokemonList.tsx

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -4,14 +4,16 @@ import Pokemon from '../../interfaces/Pokemon.ts';
 import PokemonItem from '../../components/PokemonItem/PokemonItem.tsx';
 import { sentenceCase } from 'change-case';
 
+type PokemonApiItem = { name: string; url: string };
+
 const fetchPokemonData = async (): Promise<Pokemon[]> => {
   try {
     const response = await fetch(
       'https://pokeapi.co/api/v2/pokemon?limit=151&language=fr',
     );
     const data = await response.json();
-    return data.results.map((pokemon: any, index: number) => ({
-      name: pokemon.name,
+    return data.results.map((item: PokemonApiItem, index: number) => ({
+      name: item.name,
       imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
       number: index + 1,
     })) as Pokemon[];
@@ -25,7 +27,7 @@ const PokemonList: React.FC = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (): Promise<void> => {
       const updatedPokemonList = await fetchPokemonData();
       setPokemonList(updatedPokemonList);
     };
