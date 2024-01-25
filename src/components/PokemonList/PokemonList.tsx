71b2439ff9c9ebac1dc33aa54d86dc03158171e9
Damien SABO <damien.sabo@ekino.com>
2024-01-25 15:54:51 +0100

    feat: implement infinite scroll with react-infinite-scroll-component for fetching Pokémon list

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,51 +1,71 @@
 import { useState, useEffect } from 'react';
+import { sentenceCase } from 'change-case';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './PokemonList.scss';
 import Pokemon from '@/interfaces/Pokemon.ts';
 import PokemonItem from '@/components/PokemonItem/PokemonItem.tsx';
-import { sentenceCase } from 'change-case';
 
-type PokemonApiItem = { name: string; url: string };
+type PokemonApiItem = { name: string; url: string; };
 
 const { VITE_BASE_URL, VITE_IMG_BASE_URL } = import.meta.env;
 
-const fetchPokemonData = async (): Promise<Pokemon[]> => {
-  try {
-    const response = await fetch(`${VITE_BASE_URL}/pokemon?limit=151&language=fr`);
-    const data = await response.json();
-    return data.results.map((item: PokemonApiItem, index: number) => ({
-      name: item.name,
-      imageUrl: `${VITE_IMG_BASE_URL}/${index + 1}.png`,
-      number: index + 1,
-    })) as Pokemon[];
-  } catch (error) {
-    console.error('Error fetching Pokemon data:', error);
-    return [];
-  }
-};
-
 const PokemonList: React.FC = () => {
+  const [page, setPage] = useState(1);
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
+  const fetchPokemonData = async (page: number): Promise<Pokemon[]> => {
+    const limit = 50;
+    const offset = (page - 1) * limit;
+    try {
+      const response = await fetch(`${VITE_BASE_URL}/pokemon/?limit=${limit}&offset=${offset}`);
+      const data = await response.json();
+      const newItemList = data.results.map((item: PokemonApiItem, index: number) => ({
+        name: item.name,
+        imageUrl: `${VITE_IMG_BASE_URL}/${offset + index + 1}.png`,
+        number: offset + index + 1,
+      }));
+      return newItemList as Pokemon[];
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error);
+      return [];
+    }
+  };
+
+  const loadMorePokemon = async () => {
+    const newPokemonList = await fetchPokemonData(page + 1);
+    setPokemonList((prevList) => [...prevList, ...newPokemonList]);
+    setPage((prevPage) => prevPage + 1);
+  };
+
   useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      const updatedPokemonList = await fetchPokemonData();
-      setPokemonList(updatedPokemonList);
+    const fetchInitialPokemonData = async () => {
+      const newPokemonList = await fetchPokemonData(page);
+      setPokemonList(newPokemonList);
     };
-    fetchData();
+    fetchInitialPokemonData();
   }, []);
 
   return (
-    <div className="pokemon-list">
-      <ul className="pokemon-list__container">
-        {pokemonList.map((pokemon) => (
-          <PokemonItem
-            key={pokemon.name}
-            name={sentenceCase(pokemon.name)}
-            imageUrl={pokemon.imageUrl}
-            number={pokemon.number}
-          />
-        ))}
-      </ul>
+    <div className="pokemon-list" id="scrollableDiv">
+      <InfiniteScroll
+        dataLength={pokemonList.length}
+        next={loadMorePokemon}
+        hasMore={true}
+        loader={<><br /><p>Loading...</p><br /></>}
+        endMessage={<p>No more Pokémon to load.</p>}
+        height={'calc(90lvh)'}
+      >
+        <ul className="pokemon-list__container">
+          {pokemonList.map(pokemon => (
+            <PokemonItem
+              key={`${pokemon.name}-${pokemon.number}`}
+              name={sentenceCase(pokemon.name)}
+              imageUrl={pokemon.imageUrl}
+              number={pokemon.number}
+            />
+          ))}
+        </ul>
+      </InfiniteScroll>
     </div>
   );
 };
